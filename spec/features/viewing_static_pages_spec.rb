6724b18ab0cbf3213e83b7bb9dc83e052934f5c0
Padeen <paulo.deen@gmail.com>
2020-02-08 18:54:20 +0100

    Add page title Add contact page

--- spec/features/viewing_static_pages_spec.rb
+++ spec/features/viewing_static_pages_spec.rb
@@ -1,18 +1,29 @@
 require "rails_helper"
 
 RSpec.feature "Users can view static pages" do
+  let(:base_title) { "Ruby on Rails Microblog - RSpec Edition" }
+
   scenario "when visiting about page" do
     visit "/"
     expect(page).to have_content "Ruby on Rails Microblog"
+    expect(page).to have_title "Home | #{base_title}"
   end
 
   scenario "when visiting help page" do
     visit "static_pages/help"
     expect(page).to have_content "Help"
+    expect(page).to have_title "Help | #{base_title}"
   end
 
   scenario "when visiting about page" do
     visit "static_pages/about"
     expect(page).to have_content "About"
+    expect(page).to have_title "About | #{base_title}"
+  end
+
+  scenario "when visiting contact page" do
+    visit "static_pages/contact"
+    expect(page).to have_content "Contact"
+    expect(page).to have_title "Contact | #{base_title}"
   end
 end
